d7070594303f9c60f5c5058dcc80c5882fc57ebd
sajinrajan <sajinrajan082@gmail.com>
2024-02-15 12:32:20 +0530

    nav bar fenish

---
+++ script.js
@@ -0,0 +1,12 @@
+const threeBar = document.getElementById('three-bar');
+
+const hiddenParentDiv = document.getElementById('hidden-parent-div');
+const closeButton = document.getElementById('close-btn');
+
+threeBar.addEventListener("click", function() {
+    hiddenParentDiv.style.display = "block"
+  });
+
+  close.addEventListener("click", function(){
+    hiddenParentDiv.style.di
+  })
