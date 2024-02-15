1a0b319e401fcf5fd29e52e5f0d462dd1ff122e1
sajinrajan <sajinrajan082@gmail.com>
2024-02-16 01:42:09 +0530

    java slid

--- script.js
+++ script.js
@@ -7,13 +7,19 @@ threeBar.addEventListener("click", function() {
     hiddenParentDiv.style.display = "block"
   });
 
-threeBar.onclick=function () {
-  hiddenParentDiv.style.display = "block"
-  const isOpen =   hiddenParentDiv.style.contain('block')
-  hiddenParentDiv.style = isOpen
-  ?'fa-solid fa-xmark'
-  :'fa-solid fa-bars'
-};
+
+//  threeBar.onclick=function () {
+//   hiddenParentDiv.style.display = "block"
+//   const isOpen =   hiddenParentDiv.style.contain('block')
+//   hiddenParentDiv.style = isOpen
+ 
+// };
 
 
 // class
+
+
+ function close() {
+  document.getElementById("hidden-parent-div").style.width = 0;
+}
+console.log(0);
